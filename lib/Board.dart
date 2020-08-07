cc300c1d60eb93b46355959db5944e1473566970
Selvarajan Thangavel <selvarajan.thangavel@d4t4solutions.com>
2020-08-08 00:03:09 +0530

    Adding app-icons and started build game-board

---
+++ lib/Board.dart
@@ -0,0 +1,37 @@
+import "package:flutter/material.dart";
+
+class Board extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    var rect = Offset.zero & size;
+    var gradient = RadialGradient(
+      center: const Alignment(0.7, -0.6),
+      radius: 0.2,
+      colors: [const Color(0xFFFFFF00), const Color(0xFF0099FF)],
+      stops: [0.4, 1.0],
+    );
+    canvas.drawRect(
+      rect,
+      Paint()..shader = gradient.createShader(rect),
+    );
+
+    drawCircle(canvas, size);
+  }
+
+  drawCircle(Canvas canvas, Size size) {
+    var paint = Paint()
+      ..color = Colors.teal
+      ..strokeWidth = 5
+      ..style = PaintingStyle.stroke
+      ..strokeCap = StrokeCap.round;
+
+    Offset center = Offset(size.width / 2, size.height / 2);
+
+    canvas.drawCircle(center, 100, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return false;
+  }
+}
